cb199d65599e30587182ed974aa5e4cdce1c4ab0
Santi <rosalessantiag0607@gmail.com>
2025-11-30 16:12:21 -0300

    portal update

--- Assets/Scripts/SpriteAnimator.cs
+++ Assets/Scripts/SpriteAnimator.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 public class SpriteAnimator : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
-     public List<Sprite> spritesToAnimate;
+    public List<Sprite> spritesToAnimate;
+    
     private void Start()
     {
         RunAnimation().Forget();
     }
-    private async UniTask RunAnimation(){
+    
+    private async UniTask RunAnimation()
+    {
+        // Wait until sprites are assigned
+        while (spritesToAnimate == null || spritesToAnimate.Count == 0)
+        {
+            await UniTask.Yield();
+        }
+        
         int count = 0;
-        while (true){
+        while (true)
+        {
             if (this == null) return;
-            if (spritesToAnimate.Count != 0) 
+            if (spritesToAnimate != null && spritesToAnimate.Count > 0) 
             {
                 spriteRenderer.sprite = spritesToAnimate[count];
                 count = (count + 1) % spritesToAnimate.Count;
