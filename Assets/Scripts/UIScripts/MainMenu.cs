e947a281cdff4336ecf423f3974e5d33f73b1a3a
lucaciriaco <lucaciriaco@gmail.com>
2025-12-08 06:54:57 -0300

    Main menu update

--- Assets/Scripts/UIScripts/MainMenu.cs
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -10,12 +10,16 @@ public class MainMenu : MonoBehaviour
 
     [SerializeField] private Button _startBtn;
     [SerializeField] private Button _optionBtn;
+    [SerializeField] private Button _creditsBtn;
     [SerializeField] private Button _exitBtn;
     [SerializeField] private Button _mainMenuBtn;
 
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _optionMenu;
+    [SerializeField] private GameObject _creditsMenu;
     [SerializeField] private GameObject _menuBackground;
+    [SerializeField] private GameObject _menuPortal;
+    [SerializeField] private GameObject _menuForeground;
 
     private void Awake()
     {
@@ -35,6 +39,7 @@ public class MainMenu : MonoBehaviour
         _optionBtn.onClick.AddListener(ShowSettingsMenu);
         _exitBtn.onClick.AddListener(ExitGame);
         _mainMenuBtn.onClick.AddListener(ShowMainMenu);
+        _creditsBtn.onClick.AddListener(ShowCreditsMenu);
     }
 
     public void ShowMenu(MenuType menu)
@@ -47,6 +52,9 @@ public class MainMenu : MonoBehaviour
             case MenuType.OptionsMenu:
                 ShowSettingsMenu();
                 break;
+            case MenuType.CreditsMenu:
+                ShowSettingsMenu();
+                break;
             case MenuType.HideAllMenus:
                 HideAllMenus();
                 break;
@@ -57,14 +65,19 @@ public class MainMenu : MonoBehaviour
         _mainMenu.SetActive(false);
         _optionMenu.SetActive(false);
         _menuBackground.SetActive(false);
+        _menuForeground.SetActive(false);
+        _menuPortal.SetActive(false);
         SceneLoader.Instance.LoadScene("Main");
     }
 
-    private void ShowMainMenu()
+    public void ShowMainMenu()
     {
         _mainMenu.SetActive(true);
         _optionMenu.SetActive(false);
         _menuBackground.SetActive(true);
+        _menuForeground.SetActive(true);
+        _creditsMenu.SetActive(false);
+        _menuPortal.SetActive(true);
     }
 
     private void ShowSettingsMenu()
@@ -72,6 +85,20 @@ public class MainMenu : MonoBehaviour
         _optionMenu.SetActive(true);
         _mainMenu.SetActive(false);
         _menuBackground.SetActive(true);
+        _menuForeground.SetActive(false);
+        _menuForeground.SetActive(false);
+        _creditsMenu.SetActive(false);
+        _menuPortal.SetActive(false);
+    }
+
+    private void ShowCreditsMenu()
+    {
+        _optionMenu.SetActive(true);
+        _mainMenu.SetActive(false);
+        _menuBackground.SetActive(true);
+        _menuForeground.SetActive(false);
+        _creditsMenu.SetActive(true);
+        _menuPortal.SetActive(false);
     }
 
     private void HideAllMenus()
@@ -79,6 +106,9 @@ public class MainMenu : MonoBehaviour
         _optionMenu.SetActive(false);
         _mainMenu.SetActive(false);
         _menuBackground.SetActive(false);
+        _menuForeground.SetActive(false);
+        _creditsMenu.SetActive(false);
+        _menuPortal.SetActive(false);
     }
 
     private void ExitGame()
@@ -91,5 +121,6 @@ public enum MenuType
 {
     MainMenu,
     OptionsMenu,
+    CreditsMenu,
     HideAllMenus,
 }
